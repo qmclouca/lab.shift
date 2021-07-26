034646f03a9d1db5d1176d7eecc429b232399d2e
qmclouca <qmclouca@gmail.com>
2021-07-25 23:33:21 -0300

    frontend routes, axios, configurations

---
+++ frontend/front-web/src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+const Home = () => (
+    <div className = "home-container">
+        <div className="home-content card-base border-radius-20">
+            <div className="home-text">
+                <h1 className="text-title"> 
+                    Form de cadastro de Ordem de Serviços
+                </h1>  
+            </div>
+        </div>
+    </div>
+);
+export default Home;
